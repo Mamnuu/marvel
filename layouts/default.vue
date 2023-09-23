a04ced2aca65bdb054d4656fba1e0a87695a4385
Mamnuu <kioko1908@gmail.com>
2023-09-23 16:14:05 -0500

    feat: layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-layout class="rounded rounded-md">
+      <v-app-bar style="color: white; background-color: black;">
+        <v-img src="../public/logo-marvel.png"></v-img>
+      </v-app-bar>
+    </v-layout>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="ma-5">
+        
+    </div>
+</template>
